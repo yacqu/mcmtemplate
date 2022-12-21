fa620e8074bb7c638fdbe3c1b8afca674cc85d1f
yacqu <yacqubyacqub@icloud.com>
2022-12-20 22:37:58 -0700

    navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Navbar, Nav, Button } from "react-bootstrap";
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css'
+
 
 function NavBar() {
-  return (
-    <div>NavBar</div>
+
+    const [show, setShow] = useState(false);
+    const showDropdown = (e)=>{
+    setShow(!show);
+    }
+    const hideDropdown = e => {
+    setShow(false);
+    }
+
+
+
+
+    return (
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#">Brand</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+          <Nav>
+            <Button variant="outline-primary">Sign Up</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
   )
 }
 
